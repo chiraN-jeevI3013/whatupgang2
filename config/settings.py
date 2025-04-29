3713db21e1d4e30eeea374b88a8d6976338fa16a
chiraN-jeevI3013 <chiran.jeevi3013@gmail.com>
2025-04-28 22:09:19 -0700

    Settings.py Debug & AllowedHosts for testing

--- config/settings.py
+++ config/settings.py
@@ -23,8 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a*a98j3c^z61#+(xhx%-3ohrp0r)@=px=fdf%v2c@@smih&m10'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = False
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.x.x']
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
