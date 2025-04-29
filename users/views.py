7219f28209b1eda442d404121e116a37a7c75543
chiraN-jeevI3013 <chiran.jeevi3013@gmail.com>
2025-04-28 21:40:21 -0700

    updated views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from teams.models import Team
 
-# Create your views here.
+def signup_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Send welcome email
+            user.email_user(
+                subject="Welcome to WhatUpGang!",
+                message="Hello Admin, welcome to WhatUpGang, your 1 stop destination for managing your team!"
+            )
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomAuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+def home_view(request):
+    teams = Team.objects.filter(created_by=request.user)
+    return render(request, 'teams/home.html', {'teams': teams})
